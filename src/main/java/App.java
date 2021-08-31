7d16df83d3e0fcc9e69174fe9f0339a34911d5a9
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-08-31 18:25:22 +0200

    actions contextClick, drag and drop

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
@@ -15,18 +16,20 @@ public class App {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize(); // maximize window to prevent some issu
-        driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select");
+        driver.get("https://jqueryui.com/droppable/");
 
-        driver.findElement(By.cssSelector("#accept-choices")).click();
+        driver.switchTo().frame(driver.findElement(By.cssSelector("#content > iframe")));
 
-        driver.switchTo().frame(driver.findElement(By.id("iframeResult")));
-        WebElement selectable = driver.findElement(By.id("cars"));
+        WebElement draggable = driver.findElement(By.id("draggable"));
+        WebElement droppable = driver.findElement(By.id("droppable"));
 
+        Actions dragAndDrop = new Actions(driver);
+        dragAndDrop.dragAndDrop(draggable, droppable).build().perform();
 
+        Actions contextClick = new Actions(driver);
+        contextClick.contextClick().build().perform();
 
-        Select select = new Select(selectable);
-        select.selectByIndex(1);
-
-
+        Actions actionsObj = new Actions(driver);
+        actionsObj.keyDown(Keys.F1);
     }
 }
