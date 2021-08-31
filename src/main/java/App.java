761929e59e94cd8a5a0b0277f2a5bdd58a01d846
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-08-31 17:37:43 +0200

    select action, dropdown

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -14,31 +15,17 @@ public class App {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize(); // maximize window to prevent some issu
-        driver.get("https://www.w3schools.com/html/html_iframe.asp");
+        driver.get("https://www.w3schools.com/tags/tryit.asp?filename=tryhtml_select");
 
-       // String title = driver.switchTo().frame(0).findElement(By.cssSelector("#main > h1")).getText();
-       // System.out.println(title);
+        driver.findElement(By.cssSelector("#accept-choices")).click();
 
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        if(true) {
-            System.out.println("klikam");
-            driver.findElement(By.cssSelector("#accept-choices")).click();
-
-        }
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        driver.switchTo().frame(driver.findElement(By.id("iframeResult")));
+        WebElement selectable = driver.findElement(By.id("cars"));
 
 
-        WebElement iframe = driver.findElement(By.cssSelector("#main > div:nth-child(7) > iframe"));
-        driver.switchTo().frame(iframe).findElement(By.cssSelector("#topnav > div > div > a:nth-child(5)")).click();
+
+        Select select = new Select(selectable);
+        select.selectByIndex(1);
 
 
     }
