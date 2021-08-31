54ae29fa87cee45432f6599481474e112bae7a0f
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-08-31 09:24:13 +0200

    Difrence between a locators, how to get them and use

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +11,28 @@ public class App {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.wikipedia.org/");
+
+
+
+        // locators
+        //id
+        Long start = System.currentTimeMillis();
+        driver.findElement(By.id("js-link-box-en"));
+        Long end = System.currentTimeMillis();
+
+        System.out.println("The time need to get a locator by ID is : " + (end- start));
+        //xpath
+        start = System.currentTimeMillis();
+        driver.findElement(By.xpath("//*[@id=\"www-wikipedia-org\"]/div[2]/div[3]"));
+        end = System.currentTimeMillis();
+        System.out.println("The time need to get a locator by Xpath is : " + (end- start));
+        // cssSelector
+
+        start = System.currentTimeMillis();
+        driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-featured > div.central-featured-lang.lang3"));
+        end = System.currentTimeMillis();
+        System.out.println("The time need to get a locator by CSSselector is : " + (end- start));
+
         driver.close();
     }
 }
