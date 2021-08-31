9610eb9afe7c8e46d7990acd11c2cd1f0ec0d576
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-08-31 11:43:21 +0200

    Send text to a webpage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,28 +6,20 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize(); // maximize window to prevent some issu
-
         driver.get("https://www.wikipedia.org/");
 
-        WebElement englishButton = driver.findElement(By.id("js-link-box-en"));
+        WebElement searchBox = driver.findElement(By.id("searchInput"));
+        String searchStr = "Życie";
+        searchBox.sendKeys(searchStr);
 
-        englishButton.click();
+        WebElement searchButton =driver.findElement(By.cssSelector("#search-form > fieldset > button"));
+        searchButton.click();
 
-        String expectedTitle ="Welcome to Wikipedia,";
-        WebElement titleOfEnglishPage = driver.findElement(By.id("mp-welcome"));
-
-        if (titleOfEnglishPage.getText().equals(expectedTitle)){
-            System.out.println("Test has passed! Page is the English one");
-        }
-        else
-        {
-            System.out.println("Test has failed Click() was not successful");
-        }
-        driver.close();
+       // driver.close();
     }
 }
