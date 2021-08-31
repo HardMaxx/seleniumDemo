6e8141c2a80edbd64d2a28cd9d07257521d13a6d
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-08-31 10:14:40 +0200

    getting text from a webpage

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,37 +1,32 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import java.util.concurrent.TimeUnit;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         WebDriver driver = new ChromeDriver();
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://www.wikipedia.org/");
 
+        WebElement titleOfWebPage = driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-textlogo > h1 > span"));
 
+        String titleText = titleOfWebPage.getText();
 
-        // locators
-        //id
-        Long start = System.currentTimeMillis();
-        driver.findElement(By.id("js-link-box-en"));
-        Long end = System.currentTimeMillis();
+        String expectedText = "Wikipedia";
 
-        System.out.println("The time need to get a locator by ID is : " + (end- start));
-        //xpath
-        start = System.currentTimeMillis();
-        driver.findElement(By.xpath("//*[@id=\"www-wikipedia-org\"]/div[2]/div[3]"));
-        end = System.currentTimeMillis();
-        System.out.println("The time need to get a locator by Xpath is : " + (end- start));
-        // cssSelector
+        if(titleText.equals(expectedText)){
+            System.out.println("Test has passed!");
+        }
+        else {
+            System.out.println("Test did not pass!");
+            driver.close();
+            throw new Exception();
+        }
 
-        start = System.currentTimeMillis();
-        driver.findElement(By.cssSelector("#www-wikipedia-org > div.central-featured > div.central-featured-lang.lang3"));
-        end = System.currentTimeMillis();
-        System.out.println("The time need to get a locator by CSSselector is : " + (end- start));
 
         driver.close();
     }
