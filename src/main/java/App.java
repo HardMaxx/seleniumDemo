9feb229d364f4ee4c9af0ba9047c03ba91faa4c4
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-08-31 12:20:23 +0200

    extract data from a tables

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class App {
@@ -11,15 +12,21 @@ public class App {
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize(); // maximize window to prevent some issu
-        driver.get("https://www.wikipedia.org/");
+        driver.get("file:///C:/Users/Tom/Documents/GitHub/JavaSelenium/tablePage.html");
 
-        WebElement searchBox = driver.findElement(By.id("searchInput"));
-        String searchStr = "Życie";
-        searchBox.sendKeys(searchStr);
+        //System.out.println(driver.findElement(By.xpath("/html/body/table/tbody[1]/tr[2]/td[1]")).getText());
+        //System.out.println(driver.findElement(By.xpath("/html/body/table/tbody[1]/tr[1]/th[2]")).getText());
 
-        WebElement searchButton =driver.findElement(By.cssSelector("#search-form > fieldset > button"));
-        searchButton.click();
+        List<WebElement> listOfWebElements = driver.findElements(By.xpath("/html/body/table/tbody[1]/tr"));
+        List<WebElement> listOfWebElements2 = driver.findElements(By.xpath("/html/body/table/tbody[1]/tr[1]"));
+        for(WebElement element : listOfWebElements){
+            System.out.println(element.getText());
+        }
 
-       // driver.close();
+        for(WebElement element : listOfWebElements2){
+            System.out.println(element.getText());
+        }
+
+        driver.close();
     }
 }
