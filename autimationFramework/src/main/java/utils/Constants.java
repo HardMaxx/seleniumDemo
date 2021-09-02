6b697e4992dabaead3e05175ee1e3c0508e3635d
HardMaxx <69862644+HardMaxx@users.noreply.github.com>
2021-09-02 14:54:09 +0200

    refactoring, creating a Constants class

---
+++ autimationFramework/src/main/java/utils/Constants.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class Constants {
+    public static final String PROP_FILENAME = "framework.properties";
+    public static final String FILE_NOT_FOUND_EXCEPTION_MESSAGE = "The Property file has not been found";
+    public static final String CHROME = "drivers.strategies.Chrome";
+    public static final String FIREFOX = "drivers.strategies.Firefox";
+    public static final String PHANTOMJS = "drivers.strategies.PhantomJs";
+
+}
